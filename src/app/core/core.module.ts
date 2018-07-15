5ea279c87553581246f5e42db6e673992b4fca9b
Alexey <GanjaGanja>
2018-07-15 20:55:34 +0600

    upgrade Angular packages, update modules structure

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './../shared/material/material.module';
 
@@ -17,6 +18,7 @@ import { NoDataComponent } from './no-data/no-data.component';
   imports: [
     CommonModule,
     FormsModule,
+    BrowserAnimationsModule,
     MaterialModule
   ],
   declarations: [
@@ -29,6 +31,7 @@ import { NoDataComponent } from './no-data/no-data.component';
     NoDataComponent
   ],
   exports: [
+    MaterialModule,
     HighlightBorderDirective,
     HeaderComponent,
     FooterComponent,
