a8f1bc8914fbb17627795183c8562ebf64f73300
Alexey <GanjaGanja>
2018-08-13 14:16:42 +0600

    add Moment.js and finish highlight-border directive

--- src/app/shared/directives/highlight-border/highlight-border.directive.ts
+++ src/app/shared/directives/highlight-border/highlight-border.directive.ts
@@ -1,19 +1,37 @@
 import { Directive, ElementRef, Renderer2, Input, AfterViewInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Directive({
   selector: '[appHighlightBorder]'
 })
 export class HighlightBorderDirective implements AfterViewInit {
-  @Input('appHighlightBorder') appHighlightBorder: string;
+  @Input('appHighlightBorder') appHighlightBorder: Date;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {
   }
 
   public ngAfterViewInit(): void {
-    this.addBorderColor(this.appHighlightBorder);
+    this.addBorderColor();
   }
 
-  private addBorderColor(color: string): void {
-    this.renderer.setStyle(this.el.nativeElement, 'border', `1px solid ${color}`);
+  private selectBorderColor(dateToCompare: Date): string {
+    const currentDate = moment();
+    const twoWeeksAgoDate = moment(currentDate).subtract(14, 'days');
+    const creationDate = moment(new Date(dateToCompare));
+
+    if (creationDate < currentDate && creationDate >= twoWeeksAgoDate) {
+      return 'green';
+    } else if (creationDate > currentDate) {
+      return 'blue';
+    } else {
+      return 'transparent';
+    }
   }
+
+  private addBorderColor(): void {
+    const borderColor = this.selectBorderColor(this.appHighlightBorder);
+
+    this.renderer.setStyle(this.el.nativeElement, 'border', `1px solid ${borderColor}`);
+  }
+
 }
